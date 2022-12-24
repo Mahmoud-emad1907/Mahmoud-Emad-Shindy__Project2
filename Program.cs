9b609a048ce9614fba11f03a81e601b0975cae05
Mahmoud-emad1907 <116375978+Mahmoud-emad1907@users.noreply.github.com>
2022-12-24 17:50:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahmoud_Emad_Shindy__Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First number"); int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter sand number"); int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = num1; i < num2; i++)
+            {
+                decimal sum = 0;
+                for (int x = 1; x < i; x++)
+                {
+                    if (i % x == 0)
+                        sum = sum + x;
+                }
+                    if (sum == i)
+                    {
+                        Console.WriteLine(i);
+                    }
+            }
+        }
+    }
+}
